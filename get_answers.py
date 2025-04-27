a41487c07180b5f6581c4af470f29a83a069bb3f
Karina Johansson <kahjohansson@gmail.com>
2025-04-27 08:59:25 -0300

    refactor: `get_answers`

--- get_answers.py
+++ get_answers.py
@@ -2,7 +2,6 @@ import argparse
 import time
 import os
 import yaml
-from itertools import repeat
 from pathlib import Path
 
 import pandas as pd
@@ -10,7 +9,8 @@ from groq import Groq
 from tqdm import tqdm
 
 
-def parse_yaml_file(file_path: Path) -> dict:
+def parse_yaml(file_path: Path) -> dict:
+    """Load YAML file."""
     with open(file_path, 'r', encoding='utf-8') as file:
         data = yaml.load(file, Loader=yaml.SafeLoader)
 
@@ -22,8 +22,8 @@ def fill_placeholders(prompt: str, values: dict) -> str:
     return prompt.format(**values)
 
 
-def get_answer(prompt: str, user_input: str, config: dict) -> str:
-    client = Groq(api_key=os.environ.get('GROQ_API_KEY'))
+def get_answer(client: Groq, prompt: str, user_input: str,
+               config: dict) -> str:
 
     messages = [{'role': 'system', 'content': prompt},
                 {'role': 'user', 'content': user_input}]
@@ -41,7 +41,8 @@ def get_answer(prompt: str, user_input: str, config: dict) -> str:
     return answer.choices[0].message.content.strip()
 
 
-def main():
+def parse_args() -> argparse.Namespace:
+    """Parse command-line arguments."""
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--dataset', type=Path, required=True,
                         help='Path to dataset in .jsonl format.')
@@ -53,21 +54,24 @@ def main():
     parser.add_argument('-c', '--config', type=Path, required=True,
                         help='Request configurations related to model, \
                         temperature, etc.')
-    parser.add_argument('-o', '--output_path', type=Path, required=True,
+    parser.add_argument('-o', '--output', type=Path, required=True,
                         help='Path to store the generations.')
     parser.add_argument('-z', '--sleep', type=int, required=False, default=2,
                         help='Seconds parameter of time.sleep.')
     parser.add_argument('-S', '--sample_size', type=int, required=False,
                         help='Size of head sample of the dataset to run.')
-    args = parser.parse_args()
 
-    with open(args.system, "r", encoding="utf-8") as f:
-        system_prompt = f.read()
+    return parser.parse_args()
 
-    with open(args.user, "r", encoding="utf-8") as f:
-        user_prompt = f.read()
 
-    requests_config = parse_yaml_file(args.config)
+def main():
+    args = parse_args()
+
+    system_prompt = args.system.read_text(encoding='utf-8')
+    user_prompt = args.user.read_text(encoding='utf-8')
+    config = parse_yaml(args.config)
+
+    client = Groq(api_key=os.getenv('GROQ_API_KEY'))
 
     df = pd.read_json(args.dataset, orient='records', lines=True,
                       encoding='utf-8')
@@ -75,21 +79,19 @@ def main():
     if args.sample_size:
         df = df.head(args.sample_size)
 
-    records = df.to_dict(orient='records')
-    user_prompts = map(fill_placeholders, repeat(user_prompt), records)
-    user_prompts = list(user_prompts)
-    df['user_prompt'] = user_prompts
+    df['user_prompt'] = [fill_placeholders(user_prompt, record) for record in \
+                         df.to_dict(orient='records')]
 
     output_records = []
-    for _, row in tqdm(df.iterrows(), total=len(df)):
-        answer = get_answer(system_prompt, row['user_prompt'], requests_config)
+    for _, row in tqdm(df.iterrows(), total=len(df), desc='Generating text'):
+        answer = get_answer(client, system_prompt,
+                            row['user_prompt'], config)
         record = {'id': row['id'], 'text': answer}
         output_records.append(record)
         time.sleep(args.sleep) # to overcome the limit of requests per minute
 
-    df_out = pd.DataFrame(output_records)
-    df_out.to_json(args.output_path, orient='records', lines=True,
-                   force_ascii=False)
+    pd.DataFrame(output_records).\
+        to_json(args.output, orient='records', lines=True, force_ascii=False)
 
 
 if __name__ == '__main__':
