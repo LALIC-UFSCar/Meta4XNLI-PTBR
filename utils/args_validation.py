e92bf2b0ef02e5e82eab236430a5cbbf3c61e7a7
Karina Johansson <kahjohansson@gmail.com>
2025-05-18 15:13:33 -0300

    refactor: args_validation

--- utils/args_validation.py
+++ utils/args_validation.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 def validate_file_extension(file_path: str, expected_extension: str) -> Path:
     path = Path(file_path)
-    if path.suffix != expected_extension:
+    if not path.name.lower().endswith(expected_extension.lower()):
         raise argparse.ArgumentTypeError(
             f"File {file_path} does not have required extension \
             '{expected_extension}'!"
